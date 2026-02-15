fdbfc9dece4cffe20faee6077deaaca547f8ba72
Ethan-Linux <eitanniv@gmail.com>
2026-02-15 23:03:22 +0000

    Fixes for the initial build

--- src/Collision_Angle_Mitigation/src/collision_angle_critic.cpp
+++ src/Collision_Angle_Mitigation/src/collision_angle_critic.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "nav2_mppi_controller/critics/collision_angle_critic.hpp"
+#include "collision_angle_critic.hpp"
 
 namespace mppi::critics
 {
