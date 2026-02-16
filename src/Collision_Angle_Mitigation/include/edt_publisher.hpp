7e5e770beee490cacce0537bf8bc4be6f8c8831f
Ethan-Linux <eitanniv@gmail.com>
2026-02-16 04:39:54 +0000

    Initial EDT creation Attempt
    Attempting to create an EDT using the cost_2D_map

---
+++ src/Collision_Angle_Mitigation/include/edt_publisher.hpp
@@ -0,0 +1,59 @@
+// Copyright (c) 2023, Your Name
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_COSTMAP_2D__EDT_PUBLISHER_HPP_
+#define NAV2_COSTMAP_2D__EDT_PUBLISHER_HPP_
+
+#include <string>
+#include <memory>
+
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "nav2_costmap_2d/costmap_2d.hpp"
+#include "nav2_util/lifecycle_node.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "opencv2/opencv.hpp"
+
+namespace nav2_costmap_2d
+{
+
+class EDTPublisher
+{
+public:
+  EDTPublisher(
+    const nav2_util::LifecycleNode::WeakPtr & parent,
+    Costmap2D * costmap,
+    const std::string & global_frame,
+    const std::string & topic_name);
+
+  ~EDTPublisher();
+
+  void on_activate();
+
+  void on_deactivate();
+
+  void publishEDT();
+
+private:
+  rclcpp_lifecycle::LifecycleNode::WeakPtr parent_;
+  Costmap2D * costmap_;
+  std::string global_frame_;
+  std::string topic_name_;
+  rclcpp::Clock::SharedPtr clock_;
+  rclcpp::Logger logger_;
+  rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::OccupancyGrid>::SharedPtr edt_pub_;
+};
+
+}  // namespace nav2_costmap_2d
+
+#endif  // NAV2_COSTMAP_2D__EDT_PUBLISHER_HPP_
---
+++ src/Collision_Angle_Mitigation/src/edt_publisher.cpp
@@ -0,0 +1,119 @@
+// Copyright (c) 2023, Your Name
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "nav2_costmap_2d/edt_publisher.hpp"
+#include "nav2_costmap_2d/cost_values.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include <memory>
+
+namespace nav2_costmap_2d
+{
+
+EDTPublisher::EDTPublisher(
+  const nav2_util::LifecycleNode::WeakPtr & parent,
+  Costmap2D * costmap,
+  const std::string & global_frame,
+  const std::string & topic_name)
+: parent_(parent), costmap_(costmap), global_frame_(global_frame), topic_name_(topic_name)
+{
+  auto node = parent.lock();
+  clock_ = node->get_clock();
+  logger_ = node->get_logger();
+
+  auto custom_qos = rclcpp::QoS(rclcpp::KeepLast(1)).transient_local().reliable();
+  edt_pub_ = node->create_publisher<nav_msgs::msg::OccupancyGrid>(topic_name_, custom_qos);
+}
+
+EDTPublisher::~EDTPublisher()
+{
+}
+
+void EDTPublisher::on_activate()
+{
+  edt_pub_->on_activate();
+}
+
+void EDTPublisher::on_deactivate()
+{
+  edt_pub_->on_deactivate();
+}
+
+void EDTPublisher::publishEDT()
+{
+  if (edt_pub_->get_subscription_count() == 0) {
+    return;
+  }
+
+  std::unique_lock<Costmap2D::mutex_t> lock(*(costmap_->getMutex()));
+
+  unsigned int width = costmap_->getSizeInCellsX();
+  unsigned int height = costmap_->getSizeInCellsY();
+
+  if (width == 0 || height == 0) {
+    return;
+  }
+
+  // 1. Create binary map from costmap
+  unsigned char * char_map = costmap_->getCharMap();
+
+  cv::Mat binary_map(height, width, CV_8UC1);
+  for (unsigned int r = 0; r < height; ++r) {
+    for (unsigned int c = 0; c < width; ++c) {
+      unsigned char cost = char_map[costmap_->getIndex(c, r)];
+      if (cost >= nav2_costmap_2d::LETHAL_OBSTACLE) {
+        binary_map.at<unsigned char>(r, c) = 0;  // Obstacle
+      } else {
+        binary_map.at<unsigned char>(r, c) = 255;  // Free space
+      }
+    }
+  }
+
+  // 2. Compute EDT
+  cv::Mat edt_map;
+  cv::distanceTransform(binary_map, edt_map, cv::DIST_L2, cv::DIST_MASK_PRECISE);
+  // edt_map is CV_32F, with distances in pixels (cells).
+
+  // 3. Create OccupancyGrid message
+  auto edt_msg = std::make_unique<nav_msgs::msg::OccupancyGrid>();
+
+  edt_msg->header.stamp = clock_->now();
+  edt_msg->header.frame_id = global_frame_;
+
+  edt_msg->info.map_load_time = edt_msg->header.stamp;
+  edt_msg->info.resolution = costmap_->getResolution();
+  edt_msg->info.width = width;
+  edt_msg->info.height = height;
+
+  double wx, wy;
+  costmap_->mapToWorld(0, 0, wx, wy);
+  edt_msg->info.origin.position.x = wx - costmap_->getResolution() / 2;
+  edt_msg->info.origin.position.y = wy - costmap_->getResolution() / 2;
+  edt_msg->info.origin.position.z = 0.0;
+  edt_msg->info.origin.orientation.w = 1.0;
+
+  edt_msg->data.resize(width * height);
+
+  // 4. Fill OccupancyGrid data from EDT.
+  // The value will be the distance in cells, capped at 100.
+  for (unsigned int j = 0; j < height; ++j) {
+    for (unsigned int i = 0; i < width; ++i) {
+      float dist_pixels = edt_map.at<float>(j, i);
+      edt_msg->data[j * width + i] = static_cast<int8_t>(std::min(100.0f, dist_pixels));
+    }
+  }
+
+  edt_pub_->publish(std::move(edt_msg));
+}
+
+}  // namespace nav2_costmap_2d
